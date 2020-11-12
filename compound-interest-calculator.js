fe64d680cae1449aa0980f04c34fa609fa2b7328
Biprajit Majumder <biprajit163@gmail.com>
2020-11-12 14:33:53 -0500

    compound interest calculator updated

--- compound-interest-calculator.js
+++ compound-interest-calculator.js
@@ -1,6 +1,6 @@
 
 
-function compoundInterest(P, r, n, t) {
+export function compoundInterest(P, r, n, t) {
   let A = 0;
   A = P * Math.pow((1 + r/n), t*n);
   return A;
--- main.js
+++ main.js
@@ -6,3 +6,11 @@ A = P * (1 + r/n)^(nt)
 - n = number of times interest applied per time period
 - t = time period 
 */
+
+
+import cmpndIntr from './compound-interest-calculator.js';
+
+
+let investment = new cmpndIntr();
+
+investment.compoundInteres(10000, .1, 2, 20);
